64af4cf797ff8641d5d8a5f97cd791350e43de6f
ArtemkaKun <zumikq78@gmail.com>
2020-03-18 17:12:26 +0100

    Start GUI check was added

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,7 +7,7 @@ public class UIController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _yearsText;
     [SerializeField] private TextMeshProUGUI _generationText;
     [SerializeField] private TextMeshProUGUI _countText;
-    
+
     [SerializeField] private TMP_InputField _coordX;
     [SerializeField] private TMP_InputField _coordZ;
     [SerializeField] private TMP_InputField _treeAge;
@@ -17,9 +17,10 @@ public class UIController : MonoBehaviour
     [SerializeField] private TMP_InputField _startForestSize;
 
     [SerializeField] private GameObject _worldController;
-    
+
     [SerializeField] private GameObject _startCanvas;
     [SerializeField] private GameObject _ingameCanvas;
+
     void Update()
     {
         _yearsText.SetText("Year " + WorldController.GetYear());
@@ -29,16 +30,23 @@ public class UIController : MonoBehaviour
 
     public void OnStartClick()
     {
-        WorldController.SetGroundLength(int.Parse(_coordX.text));
-        WorldController.SetGroundWidth(int.Parse(_coordZ.text));
-        WorldController.SetMaxAge(int.Parse(_treeAge.text));
-        WorldController.SetGenesCount(int.Parse(_genesCount.text));
-        WorldController.SetStartEnergy(int.Parse(_startEnergy.text));
-        WorldController.SetCellUsage(int.Parse(_cellUsage.text));
-        WorldController.SetStartForestSize(int.Parse(_startForestSize.text));
-        
-        _startCanvas.SetActive(false);
-        _ingameCanvas.SetActive(true);
-        _worldController.GetComponent<WorldController>().StartWorld();
+        if (_coordX.text != "" && _coordZ.text != "" && _treeAge.text != "" && _startEnergy.text != "" &&
+            _genesCount.text != "" && _cellUsage.text != "" && _startForestSize.text != "" &&
+            int.Parse(_coordX.text) != 0 && int.Parse(_coordZ.text) != 0 && int.Parse(_treeAge.text) != 0 &&
+            int.Parse(_startEnergy.text) != 0 && int.Parse(_genesCount.text) != 0 && int.Parse(_cellUsage.text) != 0 &&
+            int.Parse(_startForestSize.text) != 0)
+        {
+            WorldController.SetGroundLength(int.Parse(_coordX.text));
+            WorldController.SetGroundWidth(int.Parse(_coordZ.text));
+            WorldController.SetMaxAge(int.Parse(_treeAge.text));
+            WorldController.SetGenesCount(int.Parse(_genesCount.text));
+            WorldController.SetStartEnergy(int.Parse(_startEnergy.text));
+            WorldController.SetCellUsage(int.Parse(_cellUsage.text));
+            WorldController.SetStartForestSize(int.Parse(_startForestSize.text));
+
+            _startCanvas.SetActive(false);
+            _ingameCanvas.SetActive(true);
+            _worldController.GetComponent<WorldController>().StartWorld();
+        }
     }
 }
